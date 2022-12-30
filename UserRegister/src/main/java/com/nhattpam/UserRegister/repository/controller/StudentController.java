f06aeb9c0d27bccfd8c5b739786aeb6359c5eb06
Nhat Pham <nhat2up@gmail.com>
2022-12-30 08:59:59 +0700

    Add Display Data

--- UserRegister/src/main/java/com/nhattpam/UserRegister/repository/controller/StudentController.java
+++ UserRegister/src/main/java/com/nhattpam/UserRegister/repository/controller/StudentController.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.nhattpam.UserRegister.repository.controller;
 
-/**
- *
- * @author Admin
- */
+import com.nhattpam.UserRegister.model.Student;
+import com.nhattpam.UserRegister.repository.service.StudentService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
     
+    
+    private StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    
+    //add student
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
+    
+    //list student
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudent();
+    }
 }
--- UserRegister/src/main/java/com/nhattpam/UserRegister/repository/service/StudentService.java
+++ UserRegister/src/main/java/com/nhattpam/UserRegister/repository/service/StudentService.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.nhattpam.UserRegister.repository.service;
 
-/**
- *
- * @author Admin
- */
+import com.nhattpam.UserRegister.model.Student;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+
 public interface StudentService {
+    //add student
+    public Student saveStudent(Student student);
     
+    //list student
+    public List<Student> getAllStudent();
 }
